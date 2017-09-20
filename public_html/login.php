54a8276aff0e72229deb3ab8c453f3d8cfa7eb5e
Vinyas N S <vinyas.n.s@gmail.com>
2017-09-20 23:23:41 +0530

    Working login

--- public_html/login.php
+++ public_html/login.php
@@ -1,43 +1,60 @@
 <?php
 require 'com/config/DBHelper.php';
 session_start();
-if(!empty(filter_input(INPUT_POST, "email", FILTER_VALIDATE_EMAIL))
-    && !empty(filter_input(INPUT_POST, "password"))
-    && !empty(filter_input(INPUT_POST, "category", FILTER_SANITIZE_STRIPPED)))  {
-    $db = new DBHelper();
-    $conn = $db->getConnection();
-    $email = $conn->real_escape_string(filter_input(INPUT_POST, "email", FILTER_SANITIZE_EMAIL));
-    $password = $conn->real_escape_string(filter_input(INPUT_POST, "password"));
-    $category = $conn->real_escape_string(filter_input(INPUT_POST, "category", FILTER_SANITIZE_STRIPPED));
-    $password = sha1($password);
-
-    if($category == 'teacher') {
-        $query = "SELECT * FROM staff WHERE email='$email' AND password='$password' and activated=1";
-        $res = $conn->query($query);
-        if($res->num_rows == 1) {
-            $_SESSION['teacher_login'] = $email;
-            $_SESSION['category'] = $category;
-            header('location:portals/teacher/');
+$postdata = file_get_contents("php://input");
+if(isset($postdata) && !empty($postdata))   {
+    $request = json_decode($postdata);
+    if(!empty($request->email) && !empty($request->password) && !empty($request->category))  {
+        $db = new DBHelper();
+        $conn = $db->getConnection();
+        $email = $conn->real_escape_string($request->email);
+        $password = $conn->real_escape_string($request->password);
+        $category = $conn->real_escape_string($request->category);
+        $password = sha1($password);
+        if($category == 'teacher') {
+            $query = "SELECT * FROM staff WHERE email='$email' AND password='$password' and activated=1";
+            $res = $conn->query($query);
+            if($res->num_rows == 1) {
+                $_SESSION['teacher_login'] = $email;
+                $_SESSION['category'] = $category;
+                //header('location:portals/teacher/');
+                echo json_encode(array("success" => true, "data" => "Login success",
+                    "url" => "portals/teacher/"));
+                return;
+            }
+            else{
+                echo json_encode(array("success" => false, "data" => "Teacher Login failure"));
+                return;
+            }
         }
-        else{
-            echo "Invalid username or password";
-            return;
+        elseif ($category == 'hod')    {
+            $query = "SELECT * FROM hod WHERE email='$email' AND password='$password' and activated=1";
+            $res = $conn->query($query);
+            if($res->num_rows == 1) {
+                $_SESSION['hod_login'] = $email;
+                $_SESSION['category'] = $category;
+                //header('location:portals/hod/');
+                echo json_encode(array("success" => true, "data" => "HOD Login success",
+                    "url" => "portals/hod/"));
+                return;
+            }
+            else{
+                //echo "Invalid username or password";
+                echo json_encode(array("success" => false, "data" => "HOD : Invalid username or password"));
+                return;
+            }
         }
-    }
-    elseif ($category == 'hod')    {
-        $query = "SELECT * FROM hod WHERE email='$email' AND password='$password' and activated=1";
-        $res = $conn->query($query);
-        if($res->num_rows == 1) {
-            $_SESSION['hod_login'] = $email;
-            $_SESSION['category'] = $category;
-            header('location:portals/hod/');
-        }
-        else{
-            echo "Invalid username or password";
+        else    {
+            echo json_encode(array("success" => false, "data" => "Invalid category"));
             return;
         }
     }
     else    {
-        echo "Invalid ****ing category";
+        echo json_encode(array("success" => false, "data" => "Enter required details"));
+        return;
     }
+}
+else    {
+    echo json_encode(array("success" => false, "data" => "Empty request :("));
+    return;
 }
