9291d88a17cc0bf4809d1a7332c6421e29ce97ee
Vinayaka K V <me.vinayakakv@gmail.com>
2017-09-24 01:01:14 +0530

    Added Student navigation support

--- public_html/portals/teacher/assets/js/studentHandler.js
+++ public_html/portals/teacher/assets/js/studentHandler.js
@@ -1,53 +1,58 @@
-var app = new angular.module('studentHandler', ['ngMessages']);
+var app = new angular.module('studentHandler', []);
 app.controller('myController', function ($scope, $window, $http) {
     $scope.noOfCOs = 5;
+    $scope.re = "Default";
     $scope.subject = {
         id: "",
         CIE: [[], [], [], [], []]
     };
-    $scope.subjectList = [
-        {
-            name:"DBMS",
-            id:"CS510"
-        },
-        {
-            name:"DS",
-            id:"CS310"
-        }
-    ];
+    $scope.subjectList = [];
     $scope.studentList = [
-        {
-            usn: "4JC15CS129",
-            name: "Vinayaka K V"
-        },
-        {
-            usn: "4JC15CS130",
-            name: "Vinyas N S"
-        },
-        {
-            usn: "4JC15CS131",
-            name: "Vishakha M"
-        }
+        {usn: "4JC15CS129", name: "Vinayaka K V"},
+        {usn: "4JC15CS130", name: "Vinyas N S"},
+        {usn: "4JC15CS131", name: "Vishakha M"}
     ];
     $scope.selectedStudent = $scope.studentList[0];
-    $scope.selectedSubject = $scope.subjectList[0];
-    $scope.submitStudent = function () {
-        if ($scope.selectedSubject.id && $scope.noOfCOs > 0) {
-            $http({
-                method: 'POST',
-                url: 'addStudent.php',
-                data: {name: $scope.selectedSubject.name, cie: JSON.stringify($scope.subject.CIE)}
-            }).then(function (response) {
-                $scope.resp = response.data;
-            }, function (response) {
-                console.log(response.data, response.status);
-            })
-        }
-        else {
-            $window.alert("Insert valid name and number of COs");
+    var index = 0;
+    var length = $scope.studentList.length;
+    $scope.nextStudent = function () {
+        if (index < length - 1) {
+            $scope.selectedStudent = $scope.studentList[++index];
+            $scope.updateStudent();
         }
     };
-
+    $scope.prevStudent = function () {
+        if (index > 0) {
+            $scope.selectedStudent = $scope.studentList[--index];
+            $scope.updateStudent();
+        }
+    };
+    $scope.getSubjectList = function () {
+        $http({
+            method: 'POST',
+            url: 'getSubjectList.php'
+        }).then(function (response) {
+            $scope.subjectList = JSON.parse(angular.fromJson(response.data).data);
+            $scope.selectedSubject = $scope.subjectList[0];
+        }, function (response) {
+            var recieved = angular.fromJson(response.data);
+            alert(recieved.data);
+        });
+    };
+    $scope.updateStudent = function () {
+        // Send request to getStudentInfo.php
+        // Param usn,subcode
+        // if cie matrix already filled, display, else zeros
+    };
+    $scope.updateSubject = function () {
+        // To get max CO info
+        // Send request to getSubjectInfo.php
+        // if max CO matrix exsists, get it
+        // else redirect to subject.php
+    };
+    $scope.updateIndex = function () {
+        index = $scope.studentList.indexOf($scope.selectedStudent);
+    };
     $scope.changeCO = function () {
         $scope.subject.CIE = [[], [], [], [], []];
         for (var i = 0; i < 5; i++) {
@@ -56,12 +61,6 @@ app.controller('myController', function ($scope, $window, $http) {
             }
         }
     };
-
-    $scope.updateStudents = function () {
-        // Read $scope.selectedSubject.id
-        // Fetch the students according to their id
-        // Update $scope.studentList
-    };
-
     $scope.changeCO();
+    $scope.getSubjectList();
 });
