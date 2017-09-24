2c30a7d4b167d5855be647562e811f89e0f26649
Vinyas N S <vinyas.n.s@gmail.com>
2017-09-24 15:25:47 +0530

    implement getSubjectList.php and subject.php

--- public_html/portals/teacher/getSubjectList.php
+++ public_html/portals/teacher/getSubjectList.php
@@ -9,13 +9,14 @@ require '../../com/config/DBHelper.php';
 $db = new DBHelper();
 $conn = $db->getConnection();
 $email = $_SESSION['teacher_login'];
-$query = "SELECT SUB.subject_code, SUB.name from subjects AS SUB, sections AS SEC, staff WHERE SUB.section_id=SEC.section_id AND staff.department=SEC.department AND staff.staff_id=SUB.staff_id AND staff.email='$email'";
+$query = "SELECT SUB.subject_code, SUB.name, SUB.section_id from subjects AS SUB, sections AS SEC, staff WHERE SUB.section_id=SEC.section_id AND staff.department=SEC.department AND staff.staff_id=SUB.staff_id AND staff.email='$email'";
 $data = [];
 if($res = $conn->query( $query)) {
     $i = 0;
     while($row = $res->fetch_assoc())   {
         $data[$i]['subject_code'] = $row['subject_code'];
         $data[$i]['name'] = $row['name'];
+        $data[$i]['section_id'] = $row['section_id'];
         $i++;
     }
     echo json_encode(array("success" => true, "data" => json_encode($data)));
