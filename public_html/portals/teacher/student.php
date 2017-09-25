ebd161eed765e50b4c413f93905f24e5c9f56fcb
Vinayaka K V <me.vinayakakv@gmail.com>
2017-09-25 08:05:10 +0530

    Added Student Fetching support

--- public_html/portals/teacher/student.php
+++ public_html/portals/teacher/student.php
@@ -95,7 +95,8 @@ if (!isset($_SESSION['teacher_login'])) {
                                 <label class="control-label" for="subject">Subject Name</label>
                                 <div class="selectContainer">
                                     <select id="subject" ng-model="selectedSubject"
-                                            ng-options="x as x.name+' Section '+x.section_id for x in subjectList" class="form-control">
+                                            ng-options="x as x.name+ ' Section '+x.section_id for x in subjectList"
+                                            class="form-control" ng-change="getStudentList()">
                                     </select>
                                 </div>
                             </div>
