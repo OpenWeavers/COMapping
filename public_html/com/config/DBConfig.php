e2fb1d30cffa75f67ee38ed84d7e750afa208dc8
vinayaka <me.vinayakakv@gmail.com>
2017-11-16 19:01:16 +0530

    Switched to AdminLTE

--- public_html/com/config/DBConfig.php
+++ public_html/com/config/DBConfig.php
@@ -1,6 +1,6 @@
 <?php
 //Database configuration
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'Mysql@123');
+define('DB_USERNAME', 'vinayaka');
+define('DB_PASSWORD', 'test123');
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'cie_co_db');
